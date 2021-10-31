90954e2815e280fea482a9ee6dbb4c4945ed7e13
CiocilteaRazvan <ciociltearazvan@gmail.com>
2021-10-31 16:51:59 +0200

    Added cell and animal lists in Universe + created UniverseTest

--- src/main/java/universe/Universe.java
+++ src/main/java/universe/Universe.java
@@ -1,5 +1,12 @@
 package universe;
 
-public class Universe {
+import animal.Animal;
+import cell.Cell;
 
+import java.util.List;
+import java.util.Map;
+
+public class Universe {
+    Map<List<Integer>, Cell> cells;
+    List<Animal> animals;
 }
---
+++ src/test/java/universe/UniverseTest.java
@@ -0,0 +1,13 @@
+package universe;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class UniverseTest {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
