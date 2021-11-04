9673429826579bc837d46034df955f1a27e69759
alexandra-doris <62300754+alexandra-doris@users.noreply.github.com>
2021-11-04 13:33:12 +0200

    Change AnimalAsexual

--- src/main/java/Animal/AnimalAsexual.java
+++ src/main/java/Animal/AnimalAsexual.java
@@ -11,7 +11,7 @@ public class AnimalAsexual extends Animal{
     }
 
     public boolean reproduce(){
-        Animal child = giveBirth();
+        AnimalAsexual child = giveBirth();
         if (child != null)
             return true;
         return false;
@@ -21,11 +21,11 @@ public class AnimalAsexual extends Animal{
         return false;
     }
 
-    private Animal giveBirth(){
+    private AnimalAsexual giveBirth(){
         List<Cell> emptyCell = getListOfEmptyNeighbours();
         for(Cell c : emptyCell){
             if (c.occupyCell(this)){
-                Animal newAnimal = new AnimalAsexual(universe, c);
+                AnimalAsexual newAnimal = new AnimalAsexual(universe, c);
                 universe.addAnimal(newAnimal);
                 return newAnimal;
             }
