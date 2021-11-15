9aa7efd70c47d4106876e76d0877ef547d58b43e
CiocilteaRazvan <ciociltearazvan@gmail.com>
2021-11-15 14:19:07 +0200

    Renamed some variables in AnimalSexual

--- src/main/java/animal/AnimalSexual.java
+++ src/main/java/animal/AnimalSexual.java
@@ -4,16 +4,13 @@ import cell.Cell;
 import creator.Creator;
 import universe.Universe;
 
-import java.awt.*;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 public class AnimalSexual extends Animal{
-    Semaphore repSem = new Semaphore(1);
+    Semaphore reproductionSemaphore = new Semaphore(1);
 
     public AnimalSexual(Universe universe, Cell cell) {
         super(universe, cell);
@@ -23,18 +20,18 @@ public class AnimalSexual extends Animal{
     public boolean reproduce(){
         try {
             boolean success = false;
-            repSem.acquire();
+            reproductionSemaphore.acquire();
             if (this.isLookingForPartner()) {
                 System.out.println(animal_index + " is looking to reproduce");
                 success = findPartnerAndMate();
                 System.out.println(animal_index + " is no longer reproducing");
             }
-            repSem.release();
+            reproductionSemaphore.release();
             return success;
 
         } catch (InterruptedException e) {
             e.printStackTrace();
-            repSem.release();
+            reproductionSemaphore.release();
             return false;
         }
     }
@@ -42,17 +39,17 @@ public class AnimalSexual extends Animal{
     public boolean tryReproduction(){
         try {
             boolean didReproduce = false;
-            repSem.tryAcquire(5, TimeUnit.MILLISECONDS);
+            reproductionSemaphore.tryAcquire(5, TimeUnit.MILLISECONDS);
             if (this.isLookingForPartner()) {
                 giveBirth();
                 didReproduce = true;
             }
-            repSem.release();
+            reproductionSemaphore.release();
             return didReproduce;
 
         } catch (InterruptedException e) {
             e.printStackTrace();
-            repSem.release();
+            reproductionSemaphore.release();
             return false;
         }
     }
@@ -62,9 +59,9 @@ public class AnimalSexual extends Animal{
         lookingForPartner = false;
 
         List<Cell> emptyCell = getListOfEmptyNeighbours();
-        for(Cell c : emptyCell){
-            if (c.occupyCell(this)){
-                AnimalSexual newAnimal = new AnimalSexual(universe, c);
+        for(Cell cell : emptyCell){
+            if (cell.occupyCell(this)){
+                AnimalSexual newAnimal = new AnimalSexual(universe, cell);
                 universe.addAnimal(newAnimal);
                 return newAnimal;
             }
